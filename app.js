b988e439cdb8955b17cbb64e3befa6d41f7999a2
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-03-06 11:27:36 +0600

    setup js

---
+++ app.js
@@ -0,0 +1,9 @@
+const imagesArea = document.querySelector('.images');
+const gallery = document.querySelector('.gallery');
+const galleryHeader = document.querySelector('.gallery-header');
+const searchBtn = document.getElementById('search-btn');
+const sliderBtn = document.getElementById('create-slider');
+const sliderContainer = document.getElementById('sliders');
+// selected image 
+let sliders = [];
+
